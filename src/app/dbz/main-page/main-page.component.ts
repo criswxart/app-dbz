34429231398f3e465901abe687387afd11c38695
Cristian Godoy <criswxart@gmail.com>
2022-02-09 15:11:55 -0300

    lista de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  personajes: Personaje[]
 
-  ngOnInit(): void {
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregar( ){
+    if( this.nuevo.nombre.trim().length === 0 ){ return; }
+    console.log(this.nuevo);
+
   }
 
 }
